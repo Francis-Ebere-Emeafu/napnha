851207d516d42cf9321c090b5561f9fd103063fb
Emeafu Francis Ebere <freemandigits@gmail.com>
2020-01-25 00:58:21 +0100

    new url

--- napnha/settings/production.py
+++ napnha/settings/production.py
@@ -3,8 +3,8 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "www.napnha.coding4africa.com", "napnha.coding4africa.com", "www.napnha.org", "napnha.com"]
-
+# ALLOWED_HOSTS = ["127.0.0.1", "www.napnha.coding4africa.com", "napnha.coding4africa.com", "www.napnha.org", "napnha.com"]
+ALLOWED_HOSTS = ['127.0.0.1', 'http://napnha.emeafu.com', 'www.napnha.emeafu.com', 'napnha.emeafu.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
