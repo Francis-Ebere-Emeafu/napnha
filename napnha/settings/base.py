7b05f5c23b4be96a7f4ce9de3eeb77f0cbf7ab66
Emeafu Francis Ebere <freemandigits@gmail.com>
2020-08-08 09:57:29 +0100

    static root path

--- napnha/settings/base.py
+++ napnha/settings/base.py
@@ -146,7 +146,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "/home/freemandigit/webapps/awardees/napnha/static"
+# os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "otherstatic"),
